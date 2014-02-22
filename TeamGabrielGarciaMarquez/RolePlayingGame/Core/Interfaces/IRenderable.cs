6b9c8399f36f4750cd875f7d802835c795777113
luboganchev <luboganchev91@gmail.com>
2014-02-22 14:06:30 +0200

    Implement sprite class and add Interface IRenderable

---
+++ TeamGabrielGarciaMarquez/RolePlayingGame/Core/Interfaces/IRenderable.cs
@@ -0,0 +1,21 @@
+﻿namespace RolePlayingGame.Core.Interfaces
+{
+    using System;
+    using System.Drawing;
+
+    interface IRenderable
+    {
+        /// <summary>
+        /// Updates render object
+        /// </summary>
+        /// <param name="gameTime"></param>
+        /// <param name="elapsedTime"></param>
+        void Update(double gameTime, double elapsedTime);
+
+        /// <summary>
+        /// Draws render object
+        /// </summary>
+        /// <param name="graphics"></param>
+        void Draw(Graphics graphics);
+    }
+}
